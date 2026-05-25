86234008883e9675570dee61057f125c21d39992
KrushnaNimbekar <krushnanimbekar12@gmail.com>
2026-05-26 01:48:21 +0530

    Update train.py

--- train.py
+++ train.py
@@ -78,8 +78,8 @@ y = df['label']
 # =========================
 vectorizer = TfidfVectorizer(
     stop_words='english',
-    max_features=10000,
-    ngram_range=(1,3)
+    max_features=5000,
+    ngram_range=(1,2)
 )
 
 X_vec = vectorizer.fit_transform(X)
